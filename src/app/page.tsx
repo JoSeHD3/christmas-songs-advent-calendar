422ee15abb5113c66138bf3618d92059ea50c3cd
JoSeHD3 <sebastian.bozek3@gmail.com>
2024-12-03 13:20:53 +0100

    Changed background and moidified cards' opacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,23 @@ import Main from '@/components/Main';
 
 const Home = () => {
   return (
-    <>
+    <div
+      className='bg-cover bg-center'
+      style={{
+        backgroundImage: "url('https://wallpapercave.com/wp/wp7958855.jpg')",
+        backgroundAttachment: 'fixed',
+      }}
+    >
       <header className='bg-gray-700'>
         <p>content of header</p>
       </header>
-      <main className='bg-red-900 h-full w-full p-12'>
+      <main className='bg-red-900 bg-transparent h-full w-full p-12'>
         <Main />
       </main>
       <footer className='bg-red-900'>
         <p>content of footer</p>
       </footer>
-    </>
+    </div>
   );
 };
 
--- src/components/AdventCard.tsx
+++ src/components/AdventCard.tsx
@@ -1,6 +1,6 @@
 const AdventCard = ({ index }: { index: number }) => {
   return (
-    <div className='min-w-64 min-h-36 bg-red-900 rounded border-4 border-dashed border-gray-500 flex items-center justify-center'>
+    <div className='min-w-64 min-h-36 bg-red-900/85 rounded border-4 border-dashed border-gray-500 flex items-center justify-center'>
       <p className='text-6xl font-bold'>{index + 1}</p>
     </div>
   );
